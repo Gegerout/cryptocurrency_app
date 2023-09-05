8bb87ac809934c4b4a4d927d842fa8d0afd46911
Gegerout <selimchik06@gmail.com>
2023-09-05 22:01:10 +0300

    update

--- app/src/main/java/com/plcoding/cryptoapp/common/Constants.kt
+++ app/src/main/java/com/plcoding/cryptoapp/common/Constants.kt
@@ -3,5 +3,5 @@ package com.plcoding.cryptoapp.common
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
     const val PARAM_COIN_ID = "coinId"
-    const val BASE_ERROR_MESSAGE = BASE_ERROR_MESSAGE
+    const val BASE_ERROR_MESSAGE = "An unexpected error occured"
 }
